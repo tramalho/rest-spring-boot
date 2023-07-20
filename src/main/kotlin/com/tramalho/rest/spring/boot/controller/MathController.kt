f7f717dc105f1c42d48026c90612f9ed02c7f6a2
tramalho <thiagofacul@gmail.com>
2023-07-19 23:07:02 -0300

    58. 0606

--- src/main/kotlin/com/tramalho/rest/spring/boot/controller/MathController.kt
+++ src/main/kotlin/com/tramalho/rest/spring/boot/controller/MathController.kt
@@ -4,23 +4,75 @@ import com.tramalho.rest.spring.boot.exception.UnsupportedMathOperationException
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RestController
+import kotlin.math.sqrt
 
 @RestController
 class MathController {
 
-    @GetMapping("/sum/{numOne}/{numTwo}")
+    @GetMapping("/sum/{$NUMBER_ONE}/{$NUMBER_TWO}")
     fun sum(
-        @PathVariable(value = "numOne") numOne: String,
-        @PathVariable(value = "numTwo") numTwo: String
+        @PathVariable(value = NUMBER_ONE) numOne: String,
+        @PathVariable(value = NUMBER_TWO) numTwo: String
+    ): Double {
+        return execute(numOne, numTwo) { a, b -> a + b }
+    }
+
+    @GetMapping("/div/{$NUMBER_ONE}/{$NUMBER_TWO}")
+    fun div(
+        @PathVariable(value = NUMBER_ONE) numOne: String,
+        @PathVariable(value = NUMBER_TWO) numTwo: String
+    ): Double {
+        return execute(numOne, numTwo) { a, b -> a / b }
+    }
+
+    @GetMapping("/minus/{$NUMBER_ONE}/{$NUMBER_TWO}")
+    fun minus(
+        @PathVariable(value = NUMBER_ONE) numOne: String,
+        @PathVariable(value = NUMBER_TWO) numTwo: String
     ): Double {
 
+        val op: (a: Double, b: Double) -> Double = { a, b -> a - b }
+        return execute(numOne, numTwo, op)
+    }
+
+    @GetMapping("/multiply/{$NUMBER_ONE}/{$NUMBER_TWO}")
+    fun multiply(
+        @PathVariable(value = NUMBER_ONE) numOne: String,
+        @PathVariable(value = NUMBER_TWO) numTwo: String
+    ): Double {
+
+        val op = { a: Double, b: Double -> a * b }
+
+        return execute(numOne, numTwo, op)
+    }
+
+    @GetMapping("/sqrt/{$NUMBER_ONE}")
+    fun sqrt(
+        @PathVariable(value = NUMBER_ONE) numOne: String
+    ): Double {
+
+        val op = { a: Double, _: Double -> sqrt(a) }
+
+        return execute(numOne, operation = op)
+    }
+
+    private fun execute(
+        numOne: String,
+        numTwo: String? = null,
+        operation: (a: Double, b: Double) -> Double
+    ): Double {
         try {
             val numberOne = numOne.toDouble()
-            val numberTwo = numTwo.toDouble()
+            val numberTwo = numTwo?.toDouble() ?: 0.0
 
-            return numberOne + numberTwo
+            return operation(numberOne, numberTwo)
         } catch (e: NumberFormatException) {
             throw UnsupportedMathOperationException(e.message)
         }
+    }
+
+    private companion object {
+        private const val NUMBER_ONE = "numberOne"
+        private const val NUMBER_TWO = "numberTwo"
     }
 }
