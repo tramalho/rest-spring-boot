3efa3d84dca6613778193ba139694e21fad1eb31
tramalho <thiagofacul@gmail.com>
2023-08-07 22:04:14 -0300

    180. 2002

--- src/main/resources/db/migration/V1__rest-spring-boot_person.sql
+++ src/main/resources/db/migration/V1__rest-spring-boot_person.sql
@@ -1,10 +1,9 @@
 CREATE TABLE if not exists `person` (
-  `id` bigint NOT NULL,
+  `id` INT(10) AUTO_INCREMENT PRIMARY KEY,
   `address` varchar(255) DEFAULT NULL,
   `first_name` varchar(255) NOT NULL,
   `gender` varchar(255) DEFAULT NULL,
-  `last_name` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`id`)
+  `last_name` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 LOCK TABLES `person` WRITE;
